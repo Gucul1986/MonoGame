500c479e01cbe5e36a64d9c3e6bdb1ce8a7f6c38
kosta <kostas_karras@mail.ru>
2021-07-04 16:59:55 +1000

    Добавьте файлы проекта.

---
+++ MonoGame/Game1.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGame
+{
+	public class Game1 : Game
+	{
+		private GraphicsDeviceManager _graphics;
+		private SpriteBatch _spriteBatch;
+		Sprite sprite, screenSaver;
+		int speed = 2;
+		
+		public Game1()
+		{
+			_graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+
+			sprite = new Sprite(8 , 10);
+			screenSaver = new Sprite();
+
+			_graphics.PreferredBackBufferWidth = 1024;
+			_graphics.PreferredBackBufferHeight = 768;
+			_graphics.ToggleFullScreen();
+			IsMouseVisible = true;
+		}
+
+		protected override void Initialize()
+		{
+			// TODO: Add your initialization logic here
+			
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+			_spriteBatch = new SpriteBatch(GraphicsDevice);
+			sprite.Load(Content, "Textures\\IMG_0001");
+			sprite.spritePosition = new Vector2(0, Window.ClientBounds.Height - (sprite.spriteTexture.Height/2));
+			screenSaver.Load(Content, "Textures\\IMG_0009");
+			
+			// TODO: use this.Content to load your game content here
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+
+			// TODO: Add your update logic here
+			sprite.spritePosition.X += speed;
+
+			if (sprite.spritePosition.X > Window.ClientBounds.Width - sprite.spriteTexture.Width / 8)
+			{
+				sprite.spriteEffect = SpriteEffects.FlipHorizontally;
+				speed *= -1;
+			}
+
+			else if (sprite.spritePosition.X < 0)
+			{
+				sprite.spriteEffect = SpriteEffects.None;
+				speed *= -1;
+			}
+
+			double elapsed = gameTime.ElapsedGameTime.TotalSeconds;
+			sprite.UpdateFrame(elapsed);
+
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.Black);
+
+			_spriteBatch.Begin();
+
+			screenSaver.DrawSprite(_spriteBatch);
+
+			
+
+			sprite.DrawAnimationSprite(_spriteBatch);
+
+			_spriteBatch.End();
+			// TODO: Add your drawing code here
+
+			base.Draw(gameTime);
+		}
+	}
+}
---
+++ MonoGame/Sprite.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoGame
+{
+	class Sprite
+	{
+		// Текстура спрайта
+		public Texture2D spriteTexture;
+		// Позиция спрайта
+		public Vector2 spritePosition;
+		// Эффект спрайта
+		public SpriteEffects spriteEffect = SpriteEffects.None;
+		// количество кадров
+		int frameCount;
+		// как долго один кадр будет показ на экране (в мс)
+		double timeFrame;
+		// текущий кадр
+		int frame = 0;
+		// для расчета времени задержки показа одного кадра
+		double totalElapsed = 0;
+
+		public Sprite(int frameCount, int framesPerSec)
+		{
+			this.frameCount = frameCount;
+			timeFrame = (float)1 / framesPerSec;
+		}
+
+		public Sprite()
+		{
+
+		}
+		
+		public void UpdateFrame(double elapsed)
+		{
+			// всего пройденного времени = все пройденное время + еще пройденное время
+			totalElapsed += elapsed;
+			// если пройденное время больше времени, отведенного для показа одного кадра
+			if (totalElapsed > timeFrame)
+			{
+				// меняем кадр
+				frame++;
+				// Удалив этот оператор, спрайт исчезает с экрана ???
+				//frame = frame % (frameCount - 1);
+				if (frame > frameCount - 1)
+					frame = 0;
+				// пройденное время = 0;
+				totalElapsed = 0; ;
+			}
+		}
+
+		//выводит анимированный спрайт
+		public void DrawAnimationSprite(SpriteBatch spriteBatch)
+		{
+			//ширина кадра = ширина спрайта / количество кадров
+			int frameWidth = spriteTexture.Width / frameCount;
+			// выоста кадра = высота спрайта / 2
+			int frameHeight = spriteTexture.Height / 2;
+			// область выводимого кадра (x, y, ширина, высота)
+			Rectangle rectangle = new (frameWidth * frame, 0, frameWidth, frameHeight);
+			// вывод спрайта
+			spriteBatch.Draw (spriteTexture, spritePosition, rectangle, Color.White, 0.0f,Vector2.Zero, 1.0f, spriteEffect, 0.0f);
+
+		}
+
+		//загружает текстуру
+		public void Load (ContentManager content, String stringTexture)
+		{
+			spriteTexture = content.Load<Texture2D>(stringTexture);
+		}
+
+		// выводит статический спрайт
+		public void DrawSprite(SpriteBatch spriteBatch)
+		{
+			spriteBatch.Draw(spriteTexture, spritePosition, Color.White);
+		}
+	}
+}
